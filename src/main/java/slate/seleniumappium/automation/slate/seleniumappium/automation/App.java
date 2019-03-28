f1c566e2a3cfea595328e39c1dfe20129ed5dd0d
Algain <asuppiah@jirnexu.com>
2019-03-28 21:14:33 +0800

    Slate Automation Test : Project Initioation

---
+++ src/main/java/slate/seleniumappium/automation/slate/seleniumappium/automation/App.java
@@ -0,0 +1,13 @@
+package slate.seleniumappium.automation.slate.seleniumappium.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
